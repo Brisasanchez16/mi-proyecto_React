9c4612be4da605df0f4d7e1f631ffbc15d254090
Brisasanchez16 <brisncz@gmail.com>
2022-07-27 11:38:06 -0300

    navbar

---
+++ react-app/src/components/NavBar.js
@@ -0,0 +1,21 @@
+
+
+
+function NavBar(){
+    return(
+        <div className="container">
+            <div className="navbar">
+                <img className="logo" src="/public/logo/png" alt="logo" />
+                <ul>
+                    <li>Inicio </li>
+                    <li>Productos</li>
+                    <li>Sobre mi</li>
+                    <li>Metodos de pago</li>
+                    <li>Carrito</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
