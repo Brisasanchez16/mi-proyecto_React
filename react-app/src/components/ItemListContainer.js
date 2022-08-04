64185c0007eaac25b08ac747d3270931ae9bacac
Brisasanchez16 <brisncz@gmail.com>
2022-08-04 00:39:36 -0300

    contador

--- react-app/src/components/ItemListContainer.js
+++ react-app/src/components/ItemListContainer.js
@@ -1,10 +1,15 @@
+import ItemCount from "./ItemCount"
+
 function ItemListContainer(props){
     return(
+        <>
         <div>
             <h2 className="titulo">{props.greeting}Productos más vendidos en la semana </h2>
         </div>
+        
+        <ItemCount initial={1} stock={10}/>
+        </>
     )
-
 }
 
 export default ItemListContainer 
